ef126dedfaa6452bcf0d3d003b0b9177af47a4f9
wdx <872386515@qq.com>
2020-06-19 12:04:10 +0800

    数据库1

---
+++ src/main/java/com/xdw/springbootshardingjdbc/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.xdw.springbootshardingjdbc.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author wdx
+ * @since 2020-06-09
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
